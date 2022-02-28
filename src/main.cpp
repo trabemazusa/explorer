3605126029be0290a827177987a95df43c484139
zhleung <zihao.liang@qq.com>
2022-02-28 16:37:26 +0800

    OpenGL 4.1

--- src/main.cpp
+++ src/main.cpp
@@ -21,8 +21,8 @@ static const glm::vec3 gravity = glm::vec3(0.0f, -9.8f, 0.0f);
 int main()
 {
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
